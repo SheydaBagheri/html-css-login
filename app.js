b9d1a0eca87072aee5451402d9a3ba0a22fdde3a
Sheyda Bagheinasab <sheydabagherinasab@gmail.com>
2025-05-18 23:16:09 +0330

    create JS file

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+const $ = document;
+const loginBtn = $.querySelector('#log');
+const emailElem = $.querySelector('#Email');
+const passElem = $.querySelector('#Pass');
+
+loginBtn.addEventListener('click' , ()=>{
+    
+})
+function loginFunc(){
+    alert('your login')
+
+}
