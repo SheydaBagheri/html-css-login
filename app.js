c7018f341b5031a04dad89b6e43d4f367b4eb297
Sheyda Bagheinasab <sheydabagherinasab@gmail.com>
2025-05-26 20:20:20 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-'use strict'
+
 const $ = document;
 const loginBtn = $.querySelector('#log');
 const emailElem = $.querySelector('#Email');
 const passElem = $.querySelector('#Pass');
 
-loginBtn.addEventListener('click' , ()=>{
-    
-})
-function loginFunc(){
-    alert('your login')
+loginBtn.addEventListener('click' , (e)=>{
+    e.preventDefault();
 
+    let userData ={
+        userEmail : emailElem.value,
+        userPass : passElem.value
+    }
+    // console.log(userData)
+    fetch('https://ex-js-28188-default-rtdb.firebaseio.com/users.json',{
+        method: 'POST',
+        headers : {
+            'Content-type': 'application/json'
+        },
+        body : JSON.stringify(userData)
+
+    })
+    .then(res => console.log(res) , clearInput())
+    .catch(err => console.log(err))
+})
+
+function clearInput(){
+    emailElem.value = ''
+    passElem.value = ''
+ 
 }
