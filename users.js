d3fbc2a7d994af2da225b148ecc16b1d5f52a61a
Sheyda Bagheinasab <sheydabagherinasab@gmail.com>
2025-05-27 23:01:41 +0330

    Add files via upload

--- users.js
+++ users.js
@@ -1,11 +1,20 @@
 // onload show users login 
 const usersElem = document.querySelector('#wrap-users')
-
+const modalDe = document.querySelector('.modal')
+const deletBtn = document.querySelector('.delete-btn');
+const closeBtn = document.querySelector('.close-btn')
+let userId = null
 window.addEventListener('load' , ()=>{
+
+    allusers()
+})
+function allusers(){
+ 
     fetch('https://ex-js-28188-default-rtdb.firebaseio.com/users.json')
     .then(res=> res.json())
     .then(data =>{
     let userData = Object.entries(data)
+    userData.innerHTML =''
     userData.forEach(dataUs => {
         usersElem.insertAdjacentHTML('beforeend', 
                  `<div class="user">
@@ -13,7 +22,7 @@ window.addEventListener('load' , ()=>{
                     <h1>${dataUs[1].userEmail}</h1>
                     <h3>${dataUs[1].userPass}</h3>
                     </div>
-                    <button onclick="deleteFunc()">Delete</button>
+                    <button onclick="deleteFunc('${dataUs[0]}')">Delete</button>
                 </div> 
                 </div>
                 `
@@ -24,14 +33,31 @@ window.addEventListener('load' , ()=>{
     });
     console.log(userData)});
 
+}
+
+function deleteFunc(id){
+    userId = id
+    modalDe.style.display = 'flex'
+    console.log(id)
+}
+
+function closeModal(){
+       modalDe.style.display = 'none'
+}
+
+deletBtn.addEventListener('click', ()=>{
+
+    fetch(`https://ex-js-28188-default-rtdb.firebaseio.com/users/${userId}.json` ,{
+        method : 'DELETE'})
+        .then(res => {
+        allusers()
+        closeModal()
+
+    })
+
 
 
 
 })
 
-function deleteFunc(){
-
-
-
-
-}
+closeBtn.addEventListener('click', closeModal)
